bb7d51e7427a0fecd97d243905565e24f47ff393
Mattia Cugusi <m.cugusi287gmail.com>
2024-10-12 19:01:38 +0200

    esercizio completato

--- ArrayVoti.php
+++ ArrayVoti.php
@@ -8,32 +8,84 @@
 <body>
     <?php
        $totale = 0;
-       $studente["cognome"] = "Rossi";
-       $studente["nome"] = "Federico";
+       
+       $nomiStudenti = ["Federico", "Giulio", "Luca"];
+       $cognomiStudenti = ["Rossi", "Bianchi", "Verdi"];
+      
+    
        for ($i=0; $i<3; $i++) {
-        $studente["listaVoti"][$i] = rand(2,10);
-        $totale = $totale + $studente["listaVoti"][$i] = rand(2,10);
-       }
-       
-       $media = $totale/count($studente["listaVoti"]);
-       $media = number_format($media, 2);
-       
+        $studenti["nome"][$i] = $nomiStudenti[$i];
+        $studenti["cognome"][$i] = $cognomiStudenti[$i];
+        $voti = [rand(2,10), rand(2,10), rand(2,10)];
+        $studenti["voti"][$i] = $voti;
 
-  echo "<ul>
-              <li>Rossi</li>
-              <li>Mario</li>
+        echo "<h2>" .  $studenti["nome"][$i] . " " .  $studenti["cognome"][$i] . "</h2>";
+        
+        echo "<ul>
+              <li>" .  $studenti["cognome"][$i] . "</li>
+              <li>" . $studenti["nome"][$i] . "</li>
               <li>Lista voti:
      <ol>
-        <li>" . $studente["listaVoti"][0] . "</li>
-        <li>" . $studente["listaVoti"][1] . "</li>
-        <li>" . $studente["listaVoti"][2] . "</li>
+        <li>" . $studenti["voti"][$i][0] . "</li>
+        <li>" . $studenti["voti"][$i][1] . "</li>
+        <li>" . $studenti["voti"][$i][2] . "</li>
     </ol>
   </li>
-</ul>
-<span>La media e' " . $media . "</span>";
+</ul>";
+
+       }
+
+?>
+
+<table style="border: 1px solid black; border-collapse: collapse; text-align:center">
+<tr>
+  <td style="border: 1px solid black">Nome Studente</td>
+  <td style="border: 1px solid black">Cognome Studente</td>
+  <td style="border: 1px solid black">Media Voti</td>
+  <td style="border: 1px solid black">Voto massimo</td>
+</tr>
 
 
-    ?>
+<?php
+
+
+
+for ($i=0; $i<3; $i++){
+    $totale = 0;
+$max = 0;
+    for ($j=0; $j<3; $j++){
+        $totale = $totale + $studenti["voti"][$i][$j];
+        
+        if ( $studenti["voti"][$i][$j] > $max){
+           $max = $studenti["voti"][$i][$j];
+        }
+        
+    }
+    $media = $totale/count($studenti["voti"]);
+    $media = number_format($media, 2);
+
+    echo "<tr>
+            <td style='border: 1px solid black'>" . $studenti["nome"][$i] . "</td>
+            <td style='border: 1px solid black'>" . $studenti["cognome"][$i] . "</td>
+            <td style='border: 1px solid black'>" . $media . "</td>
+            <td style='border: 1px solid black'>" . $max . "</td>
+</tr>";
+}
+
+?>
+
+</table>
+
+
+       
+
+       
+
+  
+
+
+
+   
     
 </body>
 </html>
